b6c4c5967c204bdfc25b56e6c7c2462acf1aaeae
woteska <paradirichardpeter@gmail.com>
2023-06-13 02:05:43 +0200

    Create a workaround for the LapTableWidget rendering issue on Windows Desktop.

--- lib/widgets/lap_table.widget.dart
+++ lib/widgets/lap_table.widget.dart
@@ -11,31 +11,38 @@ class LapTableWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const headingTextStyle =
+        TextStyle(fontSize: 12, fontWeight: FontWeight.w800);
+    const dataTextStyle = TextStyle(fontSize: 12);
+
     return DataTable(
       dataRowMaxHeight: 20,
       dataRowMinHeight: 15,
       horizontalMargin: 30,
       dividerThickness: 0.5,
       headingRowHeight: 20,
-      dataTextStyle: const TextStyle(fontSize: 12),
-      headingTextStyle:
-          const TextStyle(fontSize: 12, fontWeight: FontWeight.w800),
+      // TODO: check why it is not render properly on Windows desktop, but Android and Web
+      // dataTextStyle: dataTextStyle,
+      // headingTextStyle: headingTextStyle,
       columns: const [
-        DataColumn(label: Text('#')),
-        DataColumn(label: Text('Lap')),
-        DataColumn(label: Text('Difference')),
+        // TODO: remove style settings when dataTextStyle and headingTextStyle issue is resolved
+        DataColumn(label: Text('#', style: headingTextStyle)),
+        DataColumn(label: Text('Lap', style: headingTextStyle)),
+        DataColumn(label: Text('Difference', style: headingTextStyle)),
       ],
       rows: List.generate(
         laps.length,
         (index) => DataRow(cells: [
+          // TODO: remove style settings when dataTextStyle and headingTextStyle issue is resolved
           DataCell(
-            Text(index.toString()),
+            Text(index.toString(), style: dataTextStyle),
           ),
           DataCell(
-            Text(laps[index].formatElapsed()),
+            Text(laps[index].formatElapsed(), style: dataTextStyle),
           ),
           DataCell(
-            Text(laps[index].formatPreviousLapDifference()),
+            Text(laps[index].formatPreviousLapDifference(),
+                style: dataTextStyle),
           ),
         ]),
       ),
